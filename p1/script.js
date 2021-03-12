4619fd729ca9ea9661a0149080b4ea861172e951
Bryon Justice <bryon.justice@gmail.com>
2021-03-12 16:41:55 -0500

    add component for game history and ability to clear

--- p1/script.js
+++ p1/script.js
@@ -2,25 +2,29 @@
 const Game = {
     data() {
         return {
-            playerName: 'bryon',
+            playerName: '',
+            gameStarted: false,
             playerThrow: null,
-            displayGamePlayerThrow: null,
-            displayGameOpponentThrow: null,
-            displayGameResult: null,
             isNotRock: true,
             isNotPaper: true,
             isNotScissors: true,
             opponentIsNotRock: true,
             opponentIsNotPaper: true,
             opponentIsNotScissors: true,
+            displayGameResult: '',
             isWin: false,
             isLoss: false,
             isDraw: false,
             playerScore: 0,
             computerScore: 0,
+            games: [],
         }
     },
     methods: {
+        startGame() {
+            this.playerName = this.playerName.toLowerCase();
+            this.gameStarted = true;
+        },
         gameResult() {
             // reset game status styling and hide all possible selections
             this.isDraw = false;
@@ -34,55 +38,74 @@ const Game = {
             this.opponentIsNotPaper = true;
             this.opponentIsNotScissors = true;
 
+            // timestamp the result
+            let gameFullDateTime = new Date();
+            let gameDate = gameFullDateTime.toLocaleDateString();
+            let gameTime = gameFullDateTime.toLocaleTimeString();
+            let gameResult = '';
+
             // both players throw
             let playerCurrentThrow = this.playerThrow;
             let opponentCurrentThrow = this.resultOpponentThrow();
 
             // show the player's throw
-            if (playerCurrentThrow == "Rock") {
+            if (playerCurrentThrow == 'Rock') {
                 this.isNotRock = false;
-            } else if (playerCurrentThrow == "Paper") {
+            } else if (playerCurrentThrow == 'Paper') {
                 this.isNotPaper = false;
             } else {
                 this.isNotScissors = false;
             }
 
             // show the opponent's throw
-            if (opponentCurrentThrow == "Rock") {
+            if (opponentCurrentThrow == 'Rock') {
                 this.opponentIsNotRock = false;
-            } else if (opponentCurrentThrow == "Paper") {
+            } else if (opponentCurrentThrow == 'Paper') {
                 this.opponentIsNotPaper = false;
             } else {
                 this.opponentIsNotScissors = false;
             }
 
-
             // compare handsigns and display the result of the game
             if (playerCurrentThrow === opponentCurrentThrow) {
-                this.displayGameResult = "draw! "
+                gameResult = 'draw';
+                this.displayGameResult = 'draw! '
                 this.isDraw = true;
-            } else if (playerCurrentThrow === "Rock" && opponentCurrentThrow === "Scissors") {
-                this.displayGameResult = "you won! "
+            } else if (playerCurrentThrow === 'Rock' && opponentCurrentThrow === 'Scissors') {
+                gameResult = 'win';
+                this.displayGameResult = 'you won! '
                 this.isWin = true;
                 this.playerScore = this.playerScore + 1;
-            } else if (playerCurrentThrow === "Scissors" && opponentCurrentThrow === "Paper") {
-                this.displayGameResult = "you won! "
+            } else if (playerCurrentThrow === 'Scissors' && opponentCurrentThrow === 'Paper') {
+                gameResult = 'win';
+                this.displayGameResult = 'you won! '
                 this.isWin = true;
                 this.playerScore = this.playerScore + 1;
-            } else if (playerCurrentThrow === "Paper" && opponentCurrentThrow === "Rock") {
-                this.displayGameResult = "you won! "
+            } else if (playerCurrentThrow === 'Paper' && opponentCurrentThrow === 'Rock') {
+                gameResult = 'win';
+                this.displayGameResult = 'you won! '
                 this.isWin = true;
                 this.playerScore = this.playerScore + 1;
             } else {
-                this.displayGameResult = "you lost! "
+                gameResult = 'loss';
+                this.displayGameResult = 'you lost! '
                 this.isLoss = true;
                 this.computerScore = this.computerScore + 1;
             }
 
-            // display the most recent choices and reset for another round
-            this.displayGamePlayerThrow = "Your current throw: " + playerCurrentThrow;
-            this.displayGameOpponentThrow = "Opponent throws: " + opponentCurrentThrow;
-            this.playerThrow = "";
+            // record the game for history
+            this.games.push({
+                result: gameResult.toLowerCase(),
+                playerThrow: playerCurrentThrow.toUpperCase(),
+                computerThrow: opponentCurrentThrow.toUpperCase(),
+                date: gameDate,
+                time: gameTime,
+            });
+
+            console.log(this.games)
+
+            // reset for another round
+            this.playerThrow = '';
         },
         resultOpponentThrow() {
             // randomly generate a selection: 0, 1, 2
@@ -103,9 +126,60 @@ const Game = {
         },
         randomSelection() {
             return Math.floor(Math.random() * Math.floor(3));
+        },
+        clearHistory() {
+            // reset the score
+            this.playerScore = 0;
+            this.computerScore = 0;
+            // refresh the games 
+            this.games = [];
+            console.log('clearing history');
         }
     }
 }
 
+const GameHistory = {
+    name: 'GameHistory',
+    props: {
+        playerScore: {
+            type: Number,
+            default: 0
+        },
+        computerScore: {
+            type: Number,
+            default: 0
+        },
+        result: {
+            type: String,
+        },
+        playerThrow: {
+            type: String,
+        },
+        playerName: {
+            type: String,
+        },
+        computerThrow: {
+            type: String,
+        },
+        index: {
+            type: Number,
+        },
+        date: {
+            type: String,
+        },
+        time: {
+            type: String,
+        },
+    },
+    data() {
+        return {
+        }
+    },
+    methods: {
+        // placeholder
+    },
+    template: '#game-history',
+}
+
 // Create a new Vue instance using our options
-const app = Vue.createApp(Game).mount('#app');
+const app = Vue.createApp(Game).component('game-history',GameHistory).mount('#app');
